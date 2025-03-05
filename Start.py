fc1ff69f9ae46abe27f667253f86318959837c78
Carmen Hurschler <hurscca1@students.zhaw.ch>
2025-03-05 14:55:46 +0100

    update

--- Start.py
+++ Start.py
@@ -5,8 +5,8 @@ st.title("ph-Wert Rechner")
 st.write("Willkommen beim pH-Wert Rechner! Diese App hilft dir, den pH-Wert einer Lösung zu bestimmen und herauszufinden, ob sie sauer, neutral oder basisch ist – ganz einfach und schnell!")
 
 # Button zum Wechseln zur Seite "ph-Wert Rechner"
-if st.button("Hier gehts zum Rechner"):
-    st.session_state.page = "ph-Wert Rechner"
+if st.button("Rechner"):
+    st.switch_page("pages/ph-Wert Rechner.py")
 
 # WICHTIG: Eure Emails müssen in der App erscheinen!
 
