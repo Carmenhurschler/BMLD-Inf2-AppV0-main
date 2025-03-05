83dd05b0ba96bea45061256ec09b582d71fa6e0b
Carmen Hurschler <hurscca1@students.zhaw.ch>
2025-03-05 14:33:55 +0100

    update

--- Start.py
+++ Start.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("ph-Wert Rechner")
-st.write("Hier kannst du per Eingabe von Masse und Molare Masse die Stoffmenge berechnen.")
+st.write("Willkommen beim pH-Wert Rechner! Diese App hilft dir, den pH-Wert einer Lösung zu bestimmen und herauszufinden, ob sie sauer, neutral oder basisch ist – ganz einfach und schnell!")
 if st.button("Hier gehts zum Rechner"):
     st.switch_page("pages/1_Rechner.py")  # Überprüfen Sie, ob der Pfad korrekt ist
 
