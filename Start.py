8d092277d3a20080eec0c7db1f91201eed3a9be6
Carmen Hurschler <hurscca1@students.zhaw.ch>
2025-03-05 14:41:21 +0100

    button hinzugefügt

--- Start.py
+++ Start.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("ph-Wert Rechner")
 st.write("Willkommen beim pH-Wert Rechner! Diese App hilft dir, den pH-Wert einer Lösung zu bestimmen und herauszufinden, ob sie sauer, neutral oder basisch ist – ganz einfach und schnell!")
 if st.button("Hier gehts zum Rechner"):
-    st.switch_page("pages/1_Rechner.py")  # Überprüfen Sie, ob der Pfad korrekt ist
+    st.switch_page("ph-Wert Rechner.py")  # Überprüfen Sie, ob der Pfad korrekt ist
 
 # WICHTIG: Eure Emails müssen in der App erscheinen!
 
