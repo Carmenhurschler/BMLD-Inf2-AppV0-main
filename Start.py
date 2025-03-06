31028afd303a5a3ef2834236f56951e2081dc41a
Carmen Hurschler <hurscca1@students.zhaw.ch>
2025-03-06 11:23:14 +0100

    update

--- Start.py
+++ Start.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("ph-Wert Rechner")
-st.write("Willkommen beim pH-Wert Rechner! Diese App hilft dir, den pH-Wert einer Lösung zu bestimmen und herauszufinden, ob sie sauer, neutral oder basisch ist – ganz einfach und schnell!")
+st.form("Willkommen beim pH-Wert Rechner! Diese App hilft dir, den pH-Wert einer Lösung zu bestimmen und herauszufinden, ob sie sauer, neutral oder basisch ist – ganz einfach und schnell!")
 
 # Button zum Wechseln zur Seite "ph-Wert Rechner"
 if st.button("Rechner"):
